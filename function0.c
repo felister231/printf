14b3bcf8061aea624d265d0d211af52302a3b008
felister231 <felimu2030@gmail.com>
2023-06-26 11:40:38 -0700

    This is our group project first commit

--- function0.c
+++ function0.c
@@ -1,32 +1,45 @@
 #include "main.h"
 
+/**
+ * _printf - produces output according to a format
+ * @format:is a character string
+ * Return: the number of characters printed
+ *
+ * Description:excluding the null byte used to end output to strings
+ */
 
-
-
-
-
-
-
-
-int _printf(const char *format, ...)
+int _printf(const char *format, ...) /* the prototype */
 {
-    va_list myCharacter;
-    if (format == NULL)
-    {
-        return (-1);
-    }
-    va_start(myCharacter, format);
-    int i = vprintf(format, myCharacter);
-    va_end(myCharacter);
-    return (i);
+	int i;
+	va_list myCharacter; /* the intialization of the varible */
+
+	if (format == NULL) /* checks whether the format string is null */
+	{
+		return (-1);
+	}
+	va_start(myCharacter, format);
+	i = vprintf(format, myCharacter);
+
+	va_end(myCharacter);
+
+	return (i);
 }
+
+/**
+ * main - entry point for the main
+ * Return:Always 0 (success)
+ *
+ */
 
 int main(void)
 {
-    char *name = "Felister Malitu";
-    int age = 23;
-    char *level = "university";
-    double weight = 56.56;
-    _printf("My name is %s, I am %d years old and I am currently in %s\n", name, age, level);
-    _printf("Oh, I weigh %.2f. That is some tiny details about me.\n", weight);
+	char *name = "Felister Malitu";
+	int age = 23;
+	char *level = "university";
+	double weight = 56.56;
+
+	_printf("My name is %s, I am %d years old\n", name, age);
+	_printf(" and I am currently in %s\n", level);
+	_printf("Oh, I weigh %.2f. That is some tiny details about me.\n", weight);
+	return (0);
 }
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+/* C standard header files */
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/* Function prototypes */
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
