bbb68eeb9474fc11fde805e861d02af147163777
felister231 <felimu2030@gmail.com>
2023-06-27 05:10:56 -0700

    empty

--- function0.c
+++ function0.c
@@ -24,3 +24,23 @@ int _printf(const char *format, ...) /* the prototype */
 
 	return (i);
 }
+
+/**
+* main - main entry 
+*
+* Return:Always 0(success)
+*/
+int main(void)
+{
+	
+	_printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", len, len);
+	_printf("Negative:[%d]\n", -762534);
+	 _printf("Unsigned:[%u]\n", ui);
+	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	_printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	_printf("Address:[%p]\n", addr);
+	return (0);
+}
+	
