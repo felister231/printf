1130af6e4a06fe3274360b5dc5d5fa8b0e44c6a3
Nusroh123 <oyedelenusroh@gmail.com>
2023-06-27 04:43:23 -0700

    added main function

--- function0.c
+++ function0.c
@@ -24,3 +24,30 @@ int _printf(const char *format, ...) /* the prototype */
 
 	return (i);
 }
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int len;
+	int len2;
+	unsigned int ui;
+	void *addr;
+	
+	len = _printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", len, len);
+	_printf("Negative:[%d]\n", -762534);
+	_printf("Unsigned:[%u]\n", ui);
+	_printf("Unsigned octal:[%o]\n", ui);
+	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	_printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	_printf("Address:[%p]\n", addr);
+	_printf("Len:[%d]\n", len);
+	_printf("Unknown:[%r]\n");
+
+	return (0);
+}
